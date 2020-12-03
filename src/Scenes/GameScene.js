cb3f3edeec800be1923e9d6f81a9476130fe15a7
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-03 18:38:54 -0300

    create player class

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,15 +1,17 @@
 import "phaser";
 import { Game } from "phaser";
 import config from "../Config/config";
+import Player from '../Entities/Player';
 
-let platforms, stars, bombs, player, cursors;
-let created = true
+let platforms, stars, bombs, cursors;
+let created = true;
+let create2 = true;
 let gameOptions = {
   // platform speed range, in pixels per second
-  platformSpeedRange: [150, 150],
+  platformSpeedRange: [150, 300],
   // spawn range, how far should be the rightmost platform from the right edge
   // before next platform spawns, in pixels
-  spawnRange: [80, 300],
+  spawnRange: [80, 200],
 
   // platform width range, in pixels
   platformSizeRange: [90, 180],
@@ -23,10 +25,8 @@ let gameOptions = {
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("Game");
-    
   }
   preload() {
-    
     this.load.image("sky", "../src/assets/sky.png");
     this.load.image("ground", "../src/assets/platform.png");
     this.load.image("platform", "../src/assets/plattexture.png");
@@ -36,100 +36,71 @@ export default class GameScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48,
     });
-    this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.resize()
 
+    this.resize1();
   }
   create() {
+
+    
     this.add.image(config.width / 2, config.height / 2, "sky");
     this.addedPlatforms = 0;
 
     platforms = this.physics.add.staticGroup();
-
-     // group with all active platforms.
-     this.platformGroup = this.add.group({
-
+console.log(this.anims)
+    // group with all active platforms.
+    this.platformGroup = this.add.group({
       // once a platform is removed, it's added to the pool
-      removeCallback: function(platform1){
-          platform1.scene.platformPool.add(platform1)
-      }
-  });
+      removeCallback: function (platform1) {
+        platform1.scene.platformPool.add(platform1);
+      },
+    });
 
-  // platform pool
-  this.platformPool = this.add.group({
-
+    // platform pool
+    this.platformPool = this.add.group({
       // once a platform is removed from the pool, it's added to the active platforms group
-      removeCallback: function(platform1){
-          platform1.scene.platformGroup.add(platform1)
-      }
-  });
+      removeCallback: function (platform1) {
+        platform1.scene.platformGroup.add(platform1);
+      },
+    });
 
-  this.addPlatform(game.config.width/3, game.config.width / 2, game.config.height*2/3);
-  this.addPlatform(game.config.width/2, game.config.width, game.config.height*1/3);
+    this.addPlatform(
+      game.config.width / 3,
+      game.config.width / 2,
+      (game.config.height * 2) / 3
+    );
+    this.addPlatform(
+      game.config.width / 2,
+      game.config.width,
+      (game.config.height * 1) / 3
+    );
 
+    platforms
+      .create(400, game.config.height, "ground")
+      .setScale(2)
+      .refreshBody();
 
-    platforms.create(400, game.config.height, "ground").setScale(2).refreshBody();
-
-    player = this.physics.add.sprite(game.config.width/2, game.config.height-120, "dude");
-
-    player.setBounce(0.1);
-    player.body.setGravityY(500);
+    // player = this.physics.add.sprite(game.config.width/2, game.config.height-120, "dude");
+    this.player = new Player(
+      this,
+      game.config.width / 2,
+      game.config.height - 120,
+      "dude"
+    );
     
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(player, this.platformGroup);
+    this.physics.add.collider(this.player, platforms);
+    this.physics.add.collider(this.player, this.platformGroup);
 
-    this.cameras.main.centerOn(game.config.width/2,game.config.height/2);
-    // this.cameras.main.setBounds(0, 0, 0, 0);
+    // .cameras.main.setBounds(0, 0, 4000, 4000);
 
-    // player.setCollideWorldBounds(true);
-    // player.checkCollision = { up: true, down: true, left: false, right: false };
-    //  window.game.world.setBounds(0, 0, 1920, 1920); 
-    // window.game.camera.follow(player);
-    // var cam2 = this.cameras.add(400, 0, 400, 300);
-    // cam2.startFollow(clown, player, 0.5, 0.5);
-
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", {
-        start: 0,
-        end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: "turn",
-      frames: [
-        {
-          key: "dude",
-          frame: 4,
-        },
-      ],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", {
-        start: 5,
-        end: 8,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
+    
+    // this.physics.world.bounds.height=6000
   }
 
   addPlatform(platformWidth, posX, posY) {
     this.addedPlatforms++;
     let platform1;
 
-    
     if (this.platformPool.getLength()) {
-      
       platform1 = this.platformPool.getFirst();
       platform1.x = posX;
       platform1.y = posY;
@@ -140,8 +111,13 @@ export default class GameScene extends Phaser.Scene {
       platform1.displayWidth = platformWidth;
       platform1.tileScaleX = 1 / platform1.scaleX;
     } else {
-
-      platform1 = this.add.tileSprite(posX, posY, platformWidth, 32, "platform");
+      platform1 = this.add.tileSprite(
+        posX,
+        posY,
+        platformWidth,
+        32,
+        "platform"
+      );
       this.physics.add.existing(platform1);
       platform1.body.setImmovable(true);
       platform1.body.setVelocityX(
@@ -157,83 +133,64 @@ export default class GameScene extends Phaser.Scene {
       gameOptions.spawnRange[0],
       gameOptions.spawnRange[1]
     );
-
   }
 
   update() {
-    cursors = this.input.keyboard.createCursorKeys();
-    const jumped = Phaser.Input.Keyboard.JustDown(cursors.up);
-    const jumpedW = Phaser.Input.Keyboard.JustDown(this.key_W);
-    
-    if (cursors.left.isDown || this.key_A.isDown) {
-      player.setVelocityX(-200);
-      
-      player.anims.play("left", true);
-    } else if (cursors.right.isDown || this.key_D.isDown) {
-      player.setVelocityX(200);
-      
-      player.anims.play("right", true);
-    } else {
-      player.setVelocityX(0);
-      
-      player.anims.play("turn");
-    }
-    if (jumped || jumpedW) {
-      if (player.body.touching.down) {
-        this.canJump = true;
-        player.setVelocityY(-380);
-      } else if (this.canJump) {
-        this.canJump = false;
-        player.setVelocityY(-380);
-      }
-    }
-
-    this.cameras.main.centerOn(game.config.width/2,player.y-this.game.config.height/5);
+    this.player.movements();
 
     let minDistance = game.config.width;
     // let rightmostPlatformHeight = 0;
-    
-    this.platformGroup.getChildren().forEach(function(platform){
-      let platformDistance = game.config.width - platform.x - platform.displayWidth / 2;
-      let nextPlatformWidth = Phaser.Math.Between(gameOptions.platformSizeRange[0], gameOptions.platformSizeRange[1]);
-      
-      if(platform.x - platform.displayWidth / 2 < 0 && created){
-        created = false
-        this.addPlatform(nextPlatformWidth,game.config.width+nextPlatformWidth/2,platform.y)
+    let nextPlatformWidth = Phaser.Math.Between(
+      gameOptions.platformSizeRange[0],
+      gameOptions.platformSizeRange[1]
+    );
+
+    this.platformGroup.getChildren().forEach(function (platform) {
+      if (platform.x - platform.displayWidth / 2 < 0 && created) {
+        created = false;
+        this.addPlatform(
+          nextPlatformWidth,
+          game.config.width + nextPlatformWidth / 2,
+          platform.y
+        );
       }
-      if(platform.x + platform.displayWidth / 2 < 0 ){
-        created = true
+      if (platform.x + platform.displayWidth / 2 < 0) {
+        created = true;
         this.platformGroup.killAndHide(platform);
         this.platformGroup.remove(platform);
       }
-
     }, this);
-    if(player.x <0){
-      player.x = game.config.width
-    }else if (player.x >=game.config.width) {
-      player.x = 0;
+    if (this.player.x < 0) {
+      this.player.x = game.config.width;
+    } else if (this.player.x >= game.config.width) {
+      this.player.x = 0;
     }
-    if(player.y > game.config.height){
-
+    if (this.player.y < game.config.height / 3 && create2) {
+      this.addPlatform(
+        nextPlatformWidth,
+        game.config.width + nextPlatformWidth / 2,
+        this.player.y
+      );
+      create2 = false;
     }
-
-
+    this.cameras.main.centerOn(
+      game.config.width / 2,
+      this.player.y - this.game.config.height / 6
+    );
   }
-  
-  resize(){
+
+  resize1() {
     let canvas = document.querySelector("canvas");
     let windowWidth = window.innerWidth;
     let windowHeight = window.innerHeight;
     let windowRatio = windowWidth / windowHeight;
     let gameRatio = game.config.width / game.config.height;
-    if(windowRatio < gameRatio){
-        canvas.style.width = windowWidth + "px";
-        canvas.style.height = (windowWidth / gameRatio) + "px";
+    if (windowRatio < gameRatio) {
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = windowWidth / gameRatio + "px";
+    } else {
+      canvas.style.width = windowHeight * gameRatio + "px";
+      canvas.style.height = windowHeight + "px";
     }
-    else{
-        canvas.style.width = (windowHeight * gameRatio) + "px";
-        canvas.style.height = windowHeight + "px";
-    }
-}
-
+  }
 }
