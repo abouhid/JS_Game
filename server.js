29fb680c2cd048f68d9393d554d31f8ce935d4b1
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-10 17:09:21 -0300

    server edit

--- server.js
+++ server.js
@@ -11,3 +11,4 @@ app.get('*', (req, res) => {
 
 app.listen(process.env.PORT || 3000, function(){	
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
